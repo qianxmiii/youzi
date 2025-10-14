e55a7f73af35bcbd88222f763c83f9ce57b00f0b
youziiii <youzicic@qq.com>
2025-10-15 00:43:40 +0800

    调整各地区和运输方式的费用

--- js/logistics.js
+++ js/logistics.js
@@ -17,8 +17,8 @@ const {deliveryMethodsByCountry, quickReplies} = window.data;
 window.onload = function () {
 
     // 获取下一个星期五的日期
-    // valid_date = getNextFriday();
-    valid_date = "09/26";
+    valid_date = getNextFriday();
+    // valid_date = "09/30";
 
     init(); // 初始化
     eventListener();
